2006006e5de7a80efa43eb83a24eabd03d4f6c11
Chandler DeLoach <deloachcd@g.cofc.edu>
2019-01-18 19:10:11 -0500

    Updates

---
+++ precache-manifest.f0acf500d7142f9b7db088bd66c127fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8aade420ab73fe4823ea7d80e6d3b12e",
+    "url": "/reactbouts/static/media/logo.8aade420.png"
+  },
+  {
+    "revision": "52ec1085f89fcaa9a110",
+    "url": "/reactbouts/static/js/runtime~main.52ec1085.js"
+  },
+  {
+    "revision": "25640657890366746911",
+    "url": "/reactbouts/static/js/main.25640657.chunk.js"
+  },
+  {
+    "revision": "5fb161e1637aa45e9c43",
+    "url": "/reactbouts/static/js/1.5fb161e1.chunk.js"
+  },
+  {
+    "revision": "25640657890366746911",
+    "url": "/reactbouts/static/css/main.5e99ff70.chunk.css"
+  },
+  {
+    "revision": "24e7236845860b1f53a5f7d31643eb86",
+    "url": "/reactbouts/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/reactbouts/precache-manifest.f0acf500d7142f9b7db088bd66c127fc.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/reactbouts/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
